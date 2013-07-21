13488b09ec765f875b74c70501af9c49a47e62a5
Michael Spear <github@bsd4breakfast.us>
2013-07-20 20:57:30 -0500

    there seems to be a bug in the cast vote

--- rpc/update_queue.php
+++ rpc/update_queue.php
@@ -1,28 +1,35 @@
 <?php
 	require_once('../CONFIG.php');
 
-
-	$current_hash_id = isset($_POST['id_hash']) ? $_POST['id_hash'] : NULL ;
+	//$current_hash_id = isset($_POST['id_hash']) ? $_POST['id_hash'] : FALSE ;
 	$advance_by_one = isset($_POST['advance_by_one']) ? $_POST['advance_by_one'] : FALSE ;
-	//maybe I should change advance_by_one to vote for ID
+	//$max_entropy = TRUE;
+	//may need a $_SESSION var to keep track of the current_hash
+	
 
 	$cvs = new Current_vote_stack();
+	if(!isset($_SESSION['current_hash_id'])){
+		$first = $cvs->getNext();
+		$_SESSION['current_hash_id'] = $first['id_hash'];
+	}
+	
 
 	if($advance_by_one){
-		$next = $cvs->getNext($current_hash_id);
+		$next = $cvs->getNext($_SESSION['current_hash_id']);
 	} else {
-		$next = $cvs->get($current_hash_id);
+		$next = $cvs->get($_SESSION['current_hash_id']);
 	}
 
-	if(!$next){
-		//somthing unexpected happended, acuracy is not important for this so we will just start over
-		$next = $cvs->getNext(0);
+	if($next){
+		$max_entropy = FALSE;
+	} else {
+		$max_entropy = TRUE;
 	}
 	
 	$return = array(
 		'id_hash' => $next['id_hash'],
 		'update_list' => TRUE,
-		'max_entropy' => FALSE,
+		'max_entropy' => $max_entropy,//when set to true the just dance mordal shows
 		'song1' => $next['song1_desc'],
 		'song2' => $next['song2_desc'],
 		'song3' => $next['song3_desc'],
