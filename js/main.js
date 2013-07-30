1be7016af25573595099276f30c4797b613ee78d
Michael Spear <github@bsd4breakfast.us>
2013-07-29 21:00:15 -0500

    moved some js loading stuff around to make it load removtely

---
+++ js/main.js
@@ -0,0 +1,60 @@
+$(function(){
+    $('#login_link').hide();
+    $('#skip').on('click',function(){
+        thinking_overlay();
+        update_queue(true);
+    });
+    update_queue(true);
+    window.setInterval(function(){update_queue(false)},3000);
+});
+
+function remove_overlay(){
+   $('html').removeClass("loading"); 
+}
+
+function thinking_overlay(){
+    $('.modal h1').html('Loading Next Vote');
+    $('html').addClass("loading");
+}
+
+function go_dance_overlay(){
+    $('.modal h1').html('Go Dance');
+    $('html').addClass("loading");
+}
+
+function cast_vote(vote_for){
+    thinking_overlay();
+    $.ajax({
+        url: 'rpc/cast_vote.php',
+        type: 'POST',
+        data: {vote_for: vote_for}
+    }).done(function(){
+        update_queue(true);
+        remove_overlay();//move this to a 'complete' function
+    });
+}
+
+function update_queue(advance_by_one){
+    advance_by_one = (typeof advance_by_one === 'undefined') ? false : advance_by_one;
+    advance_by_one = (advance_by_one) ? 1 : 0 ;
+    $.ajax({
+        url: 'rpc/update_queue.php',
+        type: 'POST',
+        data: { advance_by_one: advance_by_one},
+    }).done(function(result){
+        if(result.update_list){
+            thinking_overlay();
+            $('.song1 div').html(result.song1);
+            $('.song2 div').html(result.song2);
+            $('.song3 div').html(result.song3);
+            $('#vote1').html(result.vote1);
+            $('#vote2').html(result.vote2);
+            $('#vote3').html(result.vote3);
+            remove_overlay();
+        }else if(result.max_entropy){
+            go_dance_overlay();
+        }
+    }).error(function(){
+        alert('there was a problem');
+    })
+}
--- run_player.py
+++ run_player.py
@@ -5,23 +5,21 @@ import MySQLdb as mdb
 import sys
 import subprocess
 import time
+#here I should randomly select a song and put it in a que 2 times
+#here I should call the generate vote stack stored procedure 5 times
 
-#I will want to be able to interupt this proccess so the mc can do maual stuff
 def shellquote(s):
     return "'" + s.replace("'", "'\\''") + "'"
 
-while True:
+while True: #I will want to be able to interupt this proccess so the mc can do maual stuff
 	try:
-		con = mdb.connect('localhost', 'root', 'blizzard', 'web_jukebox');
+		con = mdb.connect('localhost', 'root', 'blizzard', 'web_jukebox')
 		cur = con.cursor()
 		cur.execute("SELECT `queue`.`id`, `songs`.`file_path` FROM `queue` JOIN `songs` ON `songs`.`id`=`queue`.`song_id`ORDER BY `time` ASC LIMIT 1")
 		query = cur.fetchone()#need to get the full path and file name of the music
 		queue_id = query[0]
 		audio_file = query[1]
 		sql_delete = "DELETE FROM `web_jukebox`.`queue` WHERE `queue`.`id` = " + str(queue_id)
-		print("#########################################")
-		print(audio_file + " audio_file")
-		print("#########################################")
 		cur.execute(sql_delete)
 		con.commit()
 	except mdb.Error, e:
@@ -31,5 +29,7 @@ while True:
 			con.close()
 		if audio_file:
 			subprocess.call("mplayer " + shellquote(audio_file), shell=True)
-		time.sleep(2) # this should stop a ton of errors from flodding the screen
+			audio_file = False
+		print("************-end of loop iteration-************")
+		time.sleep(1) # prevents a ton of error messages being output
 #end while loop
