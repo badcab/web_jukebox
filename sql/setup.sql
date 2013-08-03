365747cd1812aec6fe96dd9fbf809d3860adcb5d
Michael Spear <github@bsd4breakfast.us>
2013-08-02 19:49:08 -0500

    Well the sql is half working, I still cant add to the vote stack, something about the save function not existing.

--- sql/setup.sql
+++ sql/setup.sql
@@ -2,7 +2,7 @@
 SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
-CREATE DATABASE `web_jukebox` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+CREATE DATABASE IF NOT EXISTS `web_jukebox` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
 USE `web_jukebox`;
 
 CREATE TABLE IF NOT EXISTS `current_vote_stack` (
@@ -46,41 +46,43 @@ CREATE TABLE IF NOT EXISTS `songs` (
 
 ALTER TABLE `current_vote_stack`
   ADD CONSTRAINT `current_vote_stack_ibfk_1` 
-  FOREIGN KEY (`current_vote_stack`.`song1_id`) 
-  REFERENCES `songs` (`id`) 
+  FOREIGN KEY (`song1_id`) 
+  REFERENCES `songs`(`id`) 
   ON DELETE CASCADE ON UPDATE CASCADE;
 
 ALTER TABLE `current_vote_stack`
   ADD CONSTRAINT `current_vote_stack_ibfk_2` 
-  FOREIGN KEY (`current_vote_stack`.`song2_id`) 
-  REFERENCES `songs` (`id`) 
+  FOREIGN KEY (`song2_id`) 
+  REFERENCES `songs`(`id`) 
   ON DELETE CASCADE ON UPDATE CASCADE;
 
 ALTER TABLE `current_vote_stack`
   ADD CONSTRAINT `current_vote_stack_ibfk_3` 
-  FOREIGN KEY (`current_vote_stack`.`song3_id`) 
-  REFERENCES `songs` (`id`) 
+  FOREIGN KEY (`song3_id`) 
+  REFERENCES `songs`(`id`) 
   ON DELETE CASCADE ON UPDATE CASCADE;
 
 ALTER TABLE `queue`
   ADD CONSTRAINT `queue_ibfk_1` 
-  FOREIGN KEY (`queue`.`song_id`) 
-  REFERENCES `songs` (`id`) 
+  FOREIGN KEY (`song_id`) 
+  REFERENCES `songs`(`id`) 
   ON DELETE CASCADE ON UPDATE CASCADE;
 
 /*functions*/
+DROP FUNCTION IF EXISTS DELETE_VOTE_STACK;
 DELIMITER $$
-DROP FUNCTION IF EXISTS DELETE_VOTE_STACK $$
 CREATE FUNCTION DELETE_VOTE_STACK (REMOVE TIMESTAMP) RETURNS INT
+DETERMINISTIC
 BEGIN
   DELETE FROM `web_jukebox`.`current_vote_stack` WHERE `current_vote_stack`.`id_hash` = REMOVE;
   RETURN 0;
 END $$
 DELIMITER ;
 
+DROP FUNCTION IF EXISTS ADD_QUEUE;
 DELIMITER $$
-DROP FUNCTION IF EXISTS ADD_QUEUE $$
 CREATE FUNCTION ADD_QUEUE (SONG_ID INT(11)) RETURNS INT
+DETERMINISTIC
 BEGIN
   INSERT INTO `web_jukebox`.`queue` (`song_id`) VALUES (SONG_ID);
   UPDATE  `web_jukebox`.`songs` SET  `songs`.`has_played` =  '1' WHERE  `songs`.`id` = SONG_ID;
@@ -88,9 +90,10 @@ BEGIN
 END $$
 DELIMITER ;
 
+DROP FUNCTION IF EXISTS SAVE_VOTE_STACK;
 DELIMITER $$ 
-DROP FUNCTION IF EXISTS SAVE_VOTE_STACK $$
 CREATE FUNCTION SAVE_VOTE_STACK ( SONG1_ID INT(11), SONG2_ID INT(11), SONG3_ID INT(11), SONG1_ARTIST varchar(50), SONG2_ARTIST varchar(50), SONG3_ARTIST varchar(50), SONG1_NAME varchar(50), SONG2_NAME varchar(50), SONG3_NAME varchar(50)) RETURNS INT
+DETERMINISTIC
 BEGIN
   INSERT INTO `web_jukebox`.`current_vote_stack` (
     `id_hash` ,
@@ -109,26 +112,25 @@ BEGIN
 END $$
 DELIMITER ;
 
+DROP FUNCTION IF EXISTS SELECT_WINNING_SONG;
 DELIMITER $$
-DROP FUNCTION IF EXISTS SELECT_WINNING_SONG $$
 CREATE FUNCTION SELECT_WINNING_SONG ( S1_ID INT(11), S2_ID INT(11), S3_ID INT(11), S1_V INT(5), S2_V INT(5), S3_V INT(5)) RETURNS INT
+DETERMINISTIC
 BEGIN
-  IF S1_V >= S2_V THEN
-    IF S1_V >= S3_V THEN RETURN S1_ID;
-    END IF;
-  ELSEIF S2_V >= S1_V THEN
-    IF S2_V >= S3_V THEN RETURN S2_ID;
-    END IF;
-  ELSEIF S3_V >= S2_V THEN
-    IF S3_V >= S1_V THEN RETURN S3_ID;
-    END IF;
+  IF S1_V = GREATEST(S1_V,S2_V,S3_V) THEN
+    RETURN S1_ID;
+  ELSEIF S2_V = GREATEST(S1_V,S2_V,S3_V) THEN
+    RETURN S2_ID;
+  ELSEIF S3_V = GREATEST(S1_V,S2_V,S3_V) THEN
+    RETURN S3_ID;
   END IF;
 END $$
 DELIMITER ;
 
+DROP FUNCTION IF EXISTS SET_SONG;
 DELIMITER $$
-DROP FUNCTION IF EXISTS SET_SONG $$
 CREATE FUNCTION SET_SONG ( SONG_RANDOM_SELECT INT(11), SONG_RANDOM_SELECT_CATEGORY_EXPIRE INT(11)) RETURNS INT
+DETERMINISTIC
 BEGIN
   IF ISNULL(SONG_RANDOM_SELECT) THEN RETURN SONG_RANDOM_SELECT_CATEGORY_EXPIRE;
   ELSE RETURN SONG_RANDOM_SELECT;
@@ -137,8 +139,8 @@ END $$
 DELIMITER ;
 
 /*stored procedure*/
-DELIMITER $$ /*fail*/
-DROP PROCEDURE IF EXISTS web_jukebox.ADD_TO_CURRENT_VOTE_STACK $$
+DROP PROCEDURE IF EXISTS web_jukebox.ADD_TO_CURRENT_VOTE_STACK;
+DELIMITER $$
 CREATE PROCEDURE web_jukebox.ADD_TO_CURRENT_VOTE_STACK ()
 BEGIN
   SET @CATAGORY_RANDOM_SELECT =(SELECT `songs`.`category` FROM `songs` WHERE `songs`.`has_played` = 0 ORDER BY RAND() LIMIT 1);
@@ -153,20 +155,20 @@ BEGIN
   SET @SONG2_ID = SET_SONG ( @SONG2_RANDOM_SELECT, @SONG2_RANDOM_SELECT_CATEGORY_EXPIRE);
   SET @SONG3_ID = SET_SONG ( @SONG3_RANDOM_SELECT, @SONG3_RANDOM_SELECT_CATEGORY_EXPIRE);
 
-  SET @SONG1_ARTIST =(SELECT `songs`.`artist` WHERE `songs`.`id` = @SONG1_ID);
-  SET @SONG2_ARTIST =(SELECT `songs`.`artist` WHERE `songs`.`id` = @SONG2_ID);
-  SET @SONG3_ARTIST =(SELECT `songs`.`artist` WHERE `songs`.`id` = @SONG3_ID);
+  SET @SONG1_ARTIST =(SELECT `songs`.`artist` FROM `songs` WHERE `songs`.`id` = @SONG1_ID);
+  SET @SONG2_ARTIST =(SELECT `songs`.`artist` FROM `songs` WHERE `songs`.`id` = @SONG2_ID);
+  SET @SONG3_ARTIST =(SELECT `songs`.`artist` FROM `songs` WHERE `songs`.`id` = @SONG3_ID);
 
-  SET @SONG1_NAME =(SELECT `songs`.`name` WHERE `songs`.`id` = @SONG1_ID);
-  SET @SONG2_NAME =(SELECT `songs`.`name` WHERE `songs`.`id` = @SONG2_ID);
-  SET @SONG3_NAME =(SELECT `songs`.`name` WHERE `songs`.`id` = @SONG3_ID);
+  SET @SONG1_NAME =(SELECT `songs`.`name` FROM `songs` WHERE `songs`.`id` = @SONG1_ID);
+  SET @SONG2_NAME =(SELECT `songs`.`name` FROM `songs` WHERE `songs`.`id` = @SONG2_ID);
+  SET @SONG3_NAME =(SELECT `songs`.`name` FROM `songs` WHERE `songs`.`id` = @SONG3_ID);
 
   SET @RESULT = SAVE_VOTE_STACK(@SONG1_ID, @SONG2_ID, @SONG3_ID, @SONG1_ARTIST, @SONG2_ARTIST, @SONG3_ARTIST, @SONG1_NAME, @SONG2_NAME, @SONG3_NAME);
 END $$
 DELIMITER ;
 
+DROP PROCEDURE IF EXISTS web_jukebox.ADD_TO_QUEUE;
 DELIMITER $$
-DROP PROCEDURE IF EXISTS web_jukebox.ADD_TO_QUEUE $$
 CREATE PROCEDURE web_jukebox.ADD_TO_QUEUE ()
 BEGIN
   SET @OLDEST_VOTE_STACK_RECORD =(SELECT `current_vote_stack`.`id_hash` FROM `current_vote_stack` ORDER BY `current_vote_stack`.`id_hash` LIMIT 1);
@@ -181,6 +183,7 @@ BEGIN
   SET @S3_ID =(SELECT `current_vote_stack`.`vote3` FROM `current_vote_stack` WHERE `current_vote_stack`.`id_hash` = @OLDEST_VOTE_STACK_RECORD);
 
   SET @WINNING_SONG_ID = SELECT_WINNING_SONG( @S1_ID, @S2_ID, @S3_ID, @S1_V, @S2_V, @S3_V);
+/*I seems this function is not returning correctly*/
 
   SET @DELETED = DELETE_VOTE_STACK (@OLDEST_VOTE_STACK_RECORD);
   SET @ADDED = ADD_QUEUE (@WINNING_SONG_ID);
@@ -188,12 +191,12 @@ END $$
 DELIMITER ;
 
 /*triggers*/
+DROP TRIGGER IF EXISTS `web_jukebox`.`ADD_TO_CURRENT_VOTE_STACK_trigger`;
 DELIMITER $$
-DROP TRIGGER IF EXISTS `web_jukebox`.`ADD_TO_CURRENT_VOTE_STACK_trigger` $$
 CREATE TRIGGER `web_jukebox`.`ADD_TO_CURRENT_VOTE_STACK_trigger` AFTER DELETE ON `web_jukebox`.`queue`
 FOR EACH ROW
 BEGIN
-  CALL web_jukebox.ADD_TO_QUEUE ();
-  CALL web_jukebox.ADD_TO_CURRENT_VOTE_STACK ();
+  CALL web_jukebox.ADD_TO_QUEUE();
+  CALL web_jukebox.ADD_TO_CURRENT_VOTE_STACK();
 END $$
 DELIMITER ;
